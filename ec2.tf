e3af9a024e11b3a11a3f82e9b9e3bc0c22fbd32d
gk <koog0227@gmail.com>
2024-11-06 15:38:41 +0900

    sort text

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 variable "key_pair" {
-  type = string
+  type    = string
   default = "ec2-05-key"
 }
 
@@ -44,7 +44,7 @@ resource "aws_instance" "bastion_ec2" {
   instance_type               = "t2.micro"
   associate_public_ip_address = true # 공인 IP 설정
   subnet_id                   = aws_default_subnet.public_01.id
-  vpc_security_group_ids      = [ aws_security_group.bastion_sg.id ]
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   key_name                    = var.key_pair
   tags                        = { Name = "bastion-ec2" }
 }
@@ -55,23 +55,23 @@ resource "aws_instance" "nat_ec2" {
   instance_type               = "t2.micro"
   associate_public_ip_address = true # 공인 IP 설정
   subnet_id                   = aws_default_subnet.public_02.id
-  vpc_security_group_ids      = [ aws_security_group.bastion_sg.id ]
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   source_dest_check           = false # 다른 인스턴스의 트래픽을 중계하도록 허용
   key_name                    = var.key_pair
   tags                        = { Name = "nat-ec2" }
 }
 
 resource "aws_db_instance" "rds" {
-  allocated_storage = 20
-  engine = "postgres"
-  engine_version = "16.3"
-  instance_class = "db.t3.micro"
-  username = "admin"
-  password = "p@ssword"
-  skip_final_snapshot  = true # 삭제시 스냅샷 건너뛰기
-  publicly_accessible  = false # 퍼블릭 접근 허용 여부
-  vpc_security_group_ids = [ aws_security_group.rds_sg.id ]
-  db_subnet_group_name = aws_db_subnet_group.rds_sub_group.name
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "16.3"
+  instance_class         = "db.t3.micro"
+  username               = "admin"
+  password               = "p@ssword"
+  skip_final_snapshot    = true  # 삭제시 스냅샷 건너뛰기
+  publicly_accessible    = false # 퍼블릭 접근 허용 여부
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.rds_sub_group.name
   tags = {
     Name = "rds"
   }
